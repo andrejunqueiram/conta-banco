9c53f0d9eb9f827ae226e58d1a02c9b0be40f470
André Junqueira <andrejmelgaco@gmail.com>
2025-04-29 10:02:17 -0300

    Cria projeto conta bancaria terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Seja bem vindo ao Banco Java. Por favor digite seu nome: ");
+        String nome = scanner.next();
+        System.out.printf("Muito bem, %s. Agora informe a sua agência:\n", nome);
+        String agencia = scanner.next();
+        System.out.printf("Excelente, %s. Agora informe o número da sua conta:\n", nome);
+        int numero = scanner.nextInt();
+        System.out.printf("Ótimo, %s. Agora informe o saldo da sua conta:\n", nome);
+        Double saldo = scanner.nextDouble();
+        System.out.printf("Olá, %s! Obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s e já está disponível para saque.\n", nome, agencia, numero, saldo);
+        scanner.close();
     }
 }
